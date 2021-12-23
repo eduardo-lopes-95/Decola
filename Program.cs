b20eb366ae7dfeaf211365ce221b4378b8571838
Eduardo Sabino Lopes <eduardo.lopes@tenti.com.br>
2021-12-23 13:45:05 -0300

    desafios decola tech

---
+++ Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace Desafio___Decola_Tech
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Xenlongão
+            // int N = Convert.ToInt32(Console.ReadLine());
+            // while(N-- > 0)
+            // {
+            //     int val = Convert.ToInt32(Console.ReadLine());
+            //     int newval = (int)Math.Sqrt(val);        //complete com a sua lógica
+            //     int resposta = val - newval;
+            //     Console.WriteLine(resposta);
+            // }
+
+            //Quadrado Pares
+            // int n = int.Parse(Console.RadLine());
+            // int i = 0;
+            // for (i = 2; i <= n; i++)
+            // {
+            //     if (i % 2 == 0)
+            //     {
+            //         Console.WriteLine(i + "^" + 2 + " = " + i * i);
+            //     }
+            // }
+
+            //Sequencia S
+            // float c = 0;
+            //     for (int i = 1; i <= 100; i++)
+            //     {
+            //         c += 1.00f/i;
+            //     }
+            // Console.WriteLine(Math.Round(c,2));
+            
+            //I e J
+            // for (int i = 1; i <= 9; i+=2)
+            // {
+            //     for (int j = 7; j >= 5; j--)
+            //     {
+            //         Console.WriteLine($"I = {i} J = {j}");
+            //     }
+            // }
+
+            //Vai ter copa
+            // string str;
+            // while((str=Console.ReadLine())!= null)
+            // {
+
+            //     int x = int.Parse(str);
+            //     if (x == 0)
+            //     {
+            //         Console.WriteLine("vai ter copa!");
+            //     }
+            //     else
+            //     {
+            //         Console.WriteLine("vai ter duas!");
+            //     }
+            // }
+
+            //Par Impar
+            // int x = int.Parse(Console.ReadLine());
+            // if(x % 2 == 0){
+            //     Console.WriteLine(x+2);
+            // }
+            // else{
+            //     Console.WriteLine(x+1);
+            // }
+        }
+    }
+}
+
+
+// Dado de entrada:
+// No need input, check output only!
+// Saída esperada:
+// I=1 J=7
+// I=1 J=6
+// I=1 J=5
+
+// I=3 J=7
+// I=3 J=6
+// I=3 J=5
+
+// I=5 J=7
+// I=5 J=6
+// I=5 J=5
+// I=7 J=7
+// I=7 J=6
+// I=7 J=5
+// I=9 J=7
+// I=9 J=6
+// I=9 J=5
+// Sua Saída:
+// I = 1 J = 7
+// I = 1 J = 6
+// I = 1 J = 5
+// I = 3 J = 7
+// I = 3 J = 6
+// I =
